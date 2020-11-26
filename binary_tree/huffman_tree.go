f567a0368e4774a8154b36b55119a2682870fb3b
fyclxy <wx_215e95cc3f49408a8cf4c3cfcc96c042@git.code.tencent.com>
2020-11-26 17:53:31 +0800

    '哈夫曼树'

---
+++ binary_tree/huffman_tree.go
@@ -0,0 +1,28 @@
+package binary_tree
+
+// 哈夫曼树
+/*
+通过后序遍历，找出最小的两个点，组成树
+
+*/
+func buildHuffmanTree(nodes []int){
+	var node *TreeNode
+
+	n1,n2 := mixTwo()
+	// 初始两个树
+
+
+	for len(nodes) > 0 {
+
+
+
+	}
+}
+
+// 最小的两个书
+func mixTwo()(int,int){
+	return 1,2
+}
+
+
+
