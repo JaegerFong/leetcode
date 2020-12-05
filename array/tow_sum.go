1fdfd86b1095acce622df5bdd37c49e24d64468f
Jinx <2659980901@qq.com>
2020-12-05 11:08:08 +0800

    1. 两数之和

---
+++ array/tow_sum.go
@@ -0,0 +1,35 @@
+package array
+
+// 两数之和
+// 解法一：暴力循环
+// 时间复杂度：O（n^2）
+func twoSum(nums []int, target int) []int {
+	count := len(nums)
+	var idxs []int
+	for idx, val := range nums {
+		for i := idx + 1; i < count; i++ {
+			if val+nums[i] == target {
+				idxs = []int{
+					idx, i,
+				}
+
+				return idxs
+			}
+		}
+	}
+	return nil
+}
+
+// 解法二：哈希表辅助
+func twoSum2(nums []int, target int) []int {
+	targetMap := map[int]int{}
+	for idx, val := range nums {
+		if i, ok := targetMap[target-val]; ok {
+			return []int{
+				i, idx,
+			}
+		}
+		targetMap[val] = idx
+	}
+	return nil
+}
