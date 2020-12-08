e312586f8fbeb4ac71e64f88291009bf274e3542
fangyangcheng <2659980901@qq.com>
2020-12-08 10:25:42 +0800

    offer 24 反转整个链表

---
+++ link_list/common.go
@@ -0,0 +1,6 @@
+package link_list
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
---
+++ link_list/offer_24_reverse_list.go
@@ -0,0 +1,8 @@
+package link_list
+
+// 拿到节点的前一个节点赋值：node.Next = front
+// loop(head.Next) => head==nil head.Next
+
+func reverseList(head *ListNode) *ListNode {
+
+}
