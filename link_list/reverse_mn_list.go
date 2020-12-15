7e15aea0f5140d7214918e0675ac21ac33556007
yangchengfang <2659980901@qq.com>
2020-12-15 10:36:35 +0800

    反转前N个节点

---
+++ link_list/reverse_mn_list.go
@@ -0,0 +1,16 @@
+package link_list
+
+// 反转链表的前n个节点
+var head *ListNode
+
+func reverseN(list *ListNode, n int) *ListNode {
+	if n == 1 {
+		head = list.Next
+		return list
+	}
+
+	last := reverseN(list.Next, n-1)
+	list.Next.Next = list
+	list.Next = nil
+	return last
+}
