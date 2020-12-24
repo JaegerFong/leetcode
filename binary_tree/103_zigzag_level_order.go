5fc4aa6df4c3cd182bb31d7de659cb251a5e6e33
fangyangcheng <2659980901@qq.com>
2020-12-24 09:42:24 +0800

    103.锯齿层级遍历(左右,右左,左右,右左...)

---
+++ binary_tree/103_zigzag_level_order.go
@@ -0,0 +1,12 @@
+package binary_tree
+
+//给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	/*
+		1. 根节点存入切片A
+		2. 循环切片A不为空,则进入3
+		3. 判断切片A长度,进入循环,左右子树
+	*/
+
+	return nil
+}
