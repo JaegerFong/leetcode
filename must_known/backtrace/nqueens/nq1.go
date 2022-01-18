2693ac8317ae12a5809ec6bab284895bd641e9f4
yangcheng.fang <2659980901@qq.com>
2022-01-18 11:06:08 +0800

    nqueens

--- must_known/backtrace/nqueens/nq1.go
+++ must_known/backtrace/nqueens/nq1.go
@@ -1,46 +1,57 @@
 package nqueens
 
-func solveNQueens(n int) [][]string {
+import "strings"
 
+func solveNQueens(n int) [][]string {
+	bd := make([][]string, n)
+	for i := range bd {
+		bd[i] = make([]string, n)
+		for j := range bd[i] {
+			bd[i][j] = "."
+		}
+	}
+	res := [][]string{}
+	helper(0, bd, &res, n)
+	return res
 }
 
-// 路径：记录track
-// 选择列表：剩余的可选项
-// 结束条件：没有可选项了
-func backtrack(board [][]string, row int) {
-	lr := len(board)
-
-	// 结束
-	if row == lr {
+// 选择
+// 约束
+// 回溯目标
+func helper(r int, bd [][]string, res *[][]string, n int) {
+	// 到达最后一行
+	if r == n {
+		temp := make([]string, len(bd))
+		for i := 0; i < n; i++ {
+			temp[i] = strings.Join(bd[i], "")
+		}
+		*res = append(*res, temp)
 		return
 	}
 
-	lc := len(board[row])
-
-	for col := 0; col < lc; col++ {
-		// 排除不合法选择
-		if !checkNQValid(board, row, col) {
-			continue
+	//
+	for c := 0; c < n; c++ {
+		// 只进入满足条件的选项
+		if isValid(r, c, n, bd) {
+			// 选择
+			bd[r][c] = "Q"
+			// 追溯
+			helper(r+1, bd, res, n)
+			// 回撤
+			bd[r][c] = "."
 		}
-
-		// 选择
-		board[row][col] = "Q"
-
-		// 进入下一决策
-		backtrack(board, row+1)
-
-		// 撤销选择
-		board[row][col] = "."
 	}
+
 }
 
-func checkNQValid(board [][]string, row, col int) bool {
-
-	// 检查当前列是否有Q
-
-	// 检查右上方
-
-	// 检查左上方
-
+// 排除：同一列，左上角，右上角存在Q，则返回 false
+func isValid(r, c int, n int, bd [][]string) bool {
+	for i := 0; i < r; i++ {
+		for j := 0; j < n; j++ {
+			if bd[i][j] == "Q" && (j == c || i+j == r+c || i-j == r-c) {
+				return false
+			}
+		}
+	}
 	return true
 }
